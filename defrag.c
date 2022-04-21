764e30c65cecea62c6f24677390ff26aca9c2ea9
MrTehnical <69931156+MrTehnical@users.noreply.github.com>
2022-04-21 17:28:59 -0400

    added examples

--- defrag.c
+++ defrag.c
@@ -1,11 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <dirent.h>
+
+// Global Variables
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+
+
+// Thread function
+void* thread_func(void* arg){
+
+
+  return NULL;
+}
+
 
 int main(int argc, char **argv){
 
-  
+  // Command line error handling
+  if(argc != 3) return -1;
 
-  printf("Test\n");
+  struct dirent *entry;
 
+  // Set directory to user specification
+  chdir(argv[1]);
+
+  DIR *current = opendir(".");
+  int root_dir_counter = -2;
+
+  while((entry = readdir(current))){
+    if(entry->d_type == DT_DIR){
+      root_dir_counter++;
+      printf("%s is a drectory\n", entry->d_name);
+    }
+  } 
+
+  printf("%d root directories\n", root_dir_counter);
+  pthread_t tids[root_dir_counter];
+
+
+  // Start appropriate amount of threads
+  for(unsigned long i = 0; i < root_dir_counter; i ++){
+    int thread_create_errno = pthread_create(&tids[i], NULL, thread_func, (void*) i);
+    if(thread_create_errno != 0){
+      printf("Error creating thread\n Error Number: %d", thread_create_errno);
+      return -2;
+    }
+  }
+
+
+  // Wait for all threads to be done before we continue
+  for(int i = 0; i < root_dir_counter; i++){
+    pthread_join(tids[i], NULL);
+  }
 
   return 0;
 }
--- makefile
+++ makefile
@@ -3,9 +3,9 @@ CC = gcc
 TARGET = defrag
 
 all:
-	gcc -g -Wall -Werror -std=gnu11 -o -lpthread $(TARGET) $(TARGET).c
+	gcc -g -Wall -Werror -std=gnu11 -o -lpthread defrag defrag.c
 
 
 test: $(TARGET).c
-	$(CC) -g -o $(TARGET) $(TARGET).c
+	
 	gcc -g -o -lpthread defrag defrag.c
